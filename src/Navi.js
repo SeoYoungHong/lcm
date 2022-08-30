71ed22ee7a601806bca3fb4330653da3d7ea9d8d
imchangminlee <lcm1510@dgist.ac.kr>
2022-08-30 12:27:04 +0900

    ToolBar Button Link

--- src/Navi.js
+++ src/Navi.js
@@ -29,37 +29,32 @@ function Navi(props){
             <div class="bottom_menu">
                 <div>
                     <button class='button'>
-                        <img src={homeimg} width='35' height='35'></img>
-                        <br/>
-                        <Link to="/" style={{textDecoration: 'none'}}>Home</Link>
+                        <Link to="/" style={{textDecoration: 'none'}}><img src={homeimg} width='35' height='35'></img>
+                        <br/>Home</Link>
                     </button>
                 </div>
                 <div>
                     <button class='button'>
-                        <img src={aboutimg} width='35' height='35'></img>
-                        <br/>
-                        <Link to='/about' style={{textDecoration: 'none'}}>about</Link>
+                        <Link to='/about' style={{textDecoration: 'none'}}><img src={aboutimg} width='35' height='35'></img>
+                        <br/>about</Link>
                     </button>
                 </div>
                 <div>
                     <button class='button'>
-                        <img src={missionimg} width='35' height='35'></img>
-                        <br/>
-                        <Link to='/mission' style={{textDecoration: 'none'}}>mission</Link>
+                        <Link to='/mission' style={{textDecoration: 'none'}}><img src={missionimg} width='35' height='35'></img>
+                        <br/>mission</Link>
                     </button>
                 </div>
                 <div>
                     <button class='button'>
-                        <img src={challengeimg} width='35' height='35'></img>
-                        <br/>
-                        <Link to='/challenge' style={{textDecoration: 'none'}}>challenge</Link>
+                        <Link to='/challenge' style={{textDecoration: 'none'}}><img src={challengeimg} width='35' height='35'></img>
+                        <br/>challenge</Link>
                     </button>
                 </div>
                 <div>
                     <button class='button'>
-                        <img src={caresessionimg} width='35' height='35'></img>
-                        <br/>
-                        <Link to='/caresession' style={{textDecoration: 'none'}}>caresession</Link>
+                        <Link to='/caresession' style={{textDecoration: 'none'}}><img src={caresessionimg} width='35' height='35'></img>
+                        <br/>caresession</Link>
                     </button>
                 </div>
             </div>
